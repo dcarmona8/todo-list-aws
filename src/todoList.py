0943ad5e8a8eb3b07765ec3232dcf16202a190d8
Ubuntu <ubuntu@ip-172-31-89-165.ec2.internal>
2023-06-12 20:31:42 +0000

    Modificado if de Todo para que no compruebe si URL es vacia

--- src/todoList.py
+++ src/todoList.py
@@ -9,13 +9,13 @@ from botocore.exceptions import ClientError
 
 def get_table(dynamodb=None):
     if not dynamodb:
-        dynamodb = boto3.resource("dynamodb")
-    if not URL:
         URL = os.environ['ENDPOINT_OVERRIDE']
-    print('URL dynamoDB:'+URL)
-    boto3.client = functools.partial(boto3.client, endpoint_url=URL)
-    boto3.resource = functools.partial(boto3.resource,
+        if URL:
+            print('URL dynamoDB:'+URL)
+            boto3.client = functools.partial(boto3.client, endpoint_url=URL)
+            boto3.resource = functools.partial(boto3.resource,
                                                endpoint_url=URL)
+        dynamodb = boto3.resource("dynamodb")
     # fetch todo from the database
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     return table
