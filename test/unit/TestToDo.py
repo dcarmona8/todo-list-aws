f29d37919cd43cf57fb04c50bd805a86e5115aa8
Ubuntu <ubuntu@ip-172-31-89-165.ec2.internal>
2023-06-12 15:59:49 +0000

    Volver a comentar linea como estaba en su origen

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -68,8 +68,8 @@ class TestDatabaseFunctions(unittest.TestCase):
         print ('Response put_item:' + str(response))
         self.assertEqual(200, response['statusCode'])
         # Table mock descomento dos lineas
-        self.assertEqual(200, put_item(self.text, self.dynamodb)[
-                         'ResponseMetadata']['HTTPStatusCode'])
+        # self.assertEqual(200, put_item(self.text, self.dynamodb)[
+        #                  'ResponseMetadata']['HTTPStatusCode'])
         print ('End: test_put_todo')
 
     def test_put_todo_error(self):
