47a513bdf1d5dc7a15725e830e629c4d32aec53f
Ubuntu <ubuntu@ip-172-31-89-165.ec2.internal>
2023-06-12 20:39:06 +0000

    Modificado if de Todo para que no compruebe si URL es vacia

--- src/todoList.py
+++ src/todoList.py
@@ -42,6 +42,7 @@ def get_item(key, dynamodb=None):
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
+        print('---------------------------')
         print('Result getItem:'+str(result))
         if 'Item' in result:
             return result['Item']
